89e4dabdb5e420854db2d611dc06f554818f234f
Victor <Victor@DESKTOP-21R287O>
2025-05-24 12:02:17 +0200

    animacion 5.0

--- js.js
+++ js.js
@@ -5,12 +5,10 @@ const content = document.querySelector('.main-content');
 function endIntro() {
   intro.style.display = 'none';
   content.style.display = 'block';
-  document.body.style.overflow = 'auto';
 }
 
 audio.addEventListener('ended', endIntro);
 
-// Fallback si el audio tarda mucho o falla
 setTimeout(() => {
   if (intro.style.display !== 'none') {
     endIntro();
